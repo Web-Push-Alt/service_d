192d62581a8b235513352193fa3eaf3ce8f62d0a
Web-Push <cloudlearning777@gmail.com>
2016-05-27 19:28:51 +0900

    Update cookie-util.js

--- js/cookie-util.js
+++ js/cookie-util.js
@@ -1,44 +1,6 @@
 var COOKIE_NAME = "web-push";
 var COOKIE_EXPIRATION_DATE = 1;
-var docCookies = {
-  getItem: function (sKey) {
-    if (!sKey) { return null; }
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-  },
-  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toUTCString();
-          break;
-      }
-    }
-    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-    return true;
-  },
-  removeItem: function (sKey, sPath, sDomain) {
-    if (!this.hasItem(sKey)) { return false; }
-    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-    return true;
-  },
-  hasItem: function (sKey) {
-    if (!sKey) { return false; }
-    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-  },
-  keys: function () {
-    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-    return aKeys;
-  }
-};
+
 /** クッキーの書き込み */
 function writeCookie(aValue, aName, aExpires) {
     var value = aValue;
@@ -59,59 +21,13 @@ function writeCookie(aValue, aName, aExpires) {
             var clear_time = new Date(nowtime + (60 * 60 * 24 * 1000 * COOKIE_EXPIRATION_DATE));
             expires = clear_time.toGMTString();
         }
-        var nameVal = escape(value);
 
         // クッキーの発行（書き込み）
-        var domain = '.github.io';
-        var encDomain = encodeURIComponent(domain);
-        docCookies.setItem(name, value, expires, '/', domain, true);
-        setCookie(name, value, 365, '/', 'web-push-alt.github.io', false);
-        //document.cookie = name + "=" + escape(value) + '; expires=' + expires  +'; domain=.github.io' + '; path=/';
-        chrome.cookies.set({
-                "url": "https://web-push-alt.github.io/",
-                "name": name,
-                "value": nameVal,
-                "domain": ".github.io",
-                "path":"/",
-                "secure":true,
-                "expirationDateOptional":expires
-            }, function (cookie) {
-                console.log(JSON.stringify(cookie));
-                console.log(chrome.extension.lastError);
-               console.log(chrome.runtime.lastError);
-            }
-        );
-        
-        docCookies.getItem(COOKIE_NAME);
+        document.cookie = name + "=" + escape(value) + "; expires=" + expires + '; path=/';
         return true;
     } else {
         return false;
     }
-}
-
-function setCookie(name, value, day, path, domain, secure){
-    /**
-     * 機能：クッキーを焼く（セットする）
-     * 引数：name:キー（必須）
-     * 引数：value:値 （必須）
-     * 引数：day:クッキーの有効日数（省略時はセッションクッキー）
-     * 引数：path:クッキーを書き込むPath（省略時は実行パス）
-     * 引数：domain:クッキーを書き込むDomain（省略時は実行ドメイン）
-     * 引数：secure:セキュアかどうか？（省略時はfalse）
-     * 戻値：なし
-     */
-  
-    var member = escape(name) + "=" + escape(value) + ";";
-    if (day) {
-        var exp = new Date();
-        exp.setTime(exp.getTime() + (day*24*60*60*1000));
-    }
-    var cookie = member
-        + ((day == null)     ? ""   : ("expires=" + exp.toUTCString()+";"))
-        + ((path == null)    ? ""   : ("path=" + path+";"))
-        + ((domain == null)  ? ""   : ("domain=" + domain+";"))
-        + ((secure == true)  ? "secure;" : "");
-    document.cookie = cookie;
 }
 
 /** クッキーの読み込み */
